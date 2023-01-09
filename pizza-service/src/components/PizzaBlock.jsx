0c85e26927a303bf2f3fd741899ac07c50a41514
SifiFox <dima.antonovich.1999@gmail.com>
2023-01-09 17:39:00 +0300

    fix: fix pizza name

--- pizza-service/src/components/PizzaBlock.jsx
+++ pizza-service/src/components/PizzaBlock.jsx
@@ -1,14 +1,6 @@
 import React from "react";
 
-function PizzaBlock({
-  title,
-  price,
-  imageUrl,
-  sizes,
-  types,
-  category,
-  rating,
-}) {
+function PizzaBlock({ name, price, imageUrl, sizes, types, category, rating }) {
   // const [pizzaSize, setPizzaSize] = React.useState();
 
   const [activeType, setActiveType] = React.useState(0);
@@ -19,7 +11,7 @@ function PizzaBlock({
   return (
     <div className="pizza-block">
       <img className="pizza-block__image" src={imageUrl} alt="Pizza" />
-      <h4 className="pizza-block__title">{title}</h4>
+      <h4 className="pizza-block__title">{name}</h4>
       <div className="pizza-block__selector">
         <ul>
           {types.map((type, index) => (
