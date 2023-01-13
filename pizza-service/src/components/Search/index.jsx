8ddee5490ea804a8919c0908917fa43ebc6f4b35
SifiFox <dima.antonovich.1999@gmail.com>
2023-01-13 17:59:26 +0300

    feat: add pagination; start study context hook

---
+++ pizza-service/src/components/Search/index.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+
+import styles from "./Search.module.scss";
+
+export default function Search({ searchValue, setSearchValue }) {
+  const clearInput = () => {
+    setSearchValue("");
+  };
+  return (
+    <div className={styles.root}>
+      <svg
+        className={styles.icon}
+        enableBackground="new 0 0 32 32"
+        id="Glyph"
+        version="1.1"
+        viewBox="0 0 32 32"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M27.414,24.586l-5.077-5.077C23.386,17.928,24,16.035,24,14c0-5.514-4.486-10-10-10S4,8.486,4,14  s4.486,10,10,10c2.035,0,3.928-0.614,5.509-1.663l5.077,5.077c0.78,0.781,2.048,0.781,2.828,0  C28.195,26.633,28.195,25.367,27.414,24.586z M7,14c0-3.86,3.14-7,7-7s7,3.14,7,7s-3.14,7-7,7S7,17.86,7,14z"
+          id="XMLID_223_"
+        />
+      </svg>
+      <input
+        type="text"
+        className={styles.input}
+        placeholder="Поиск пиццы..."
+        value={searchValue}
+        onChange={(e) => {
+          setSearchValue(e.target.value);
+        }}
+      />
+
+      {searchValue && (
+        <svg
+          onClick={clearInput}
+          className={styles.clearIcon}
+          id="Layer_1"
+          version="1.1"
+          viewBox="0 0 512 512"
+          width="512px"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path d="M437.5,386.6L306.9,256l130.6-130.6c14.1-14.1,14.1-36.8,0-50.9c-14.1-14.1-36.8-14.1-50.9,0L256,205.1L125.4,74.5  c-14.1-14.1-36.8-14.1-50.9,0c-14.1,14.1-14.1,36.8,0,50.9L205.1,256L74.5,386.6c-14.1,14.1-14.1,36.8,0,50.9  c14.1,14.1,36.8,14.1,50.9,0L256,306.9l130.6,130.6c14.1,14.1,36.8,14.1,50.9,0C451.5,423.4,451.5,400.6,437.5,386.6z" />
+        </svg>
+      )}
+    </div>
+  );
+}
